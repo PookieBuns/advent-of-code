22b4c7ae4d40f4447084b630d164bf6eb0424319
allenyuchen <allenyuchen@hotmail.com>
2023-12-02 22:44:24 -0800

    day_2 part_2

--- src/year_2023/day_2/mod.rs
+++ src/year_2023/day_2/mod.rs
@@ -33,8 +33,7 @@ fn is_possible(line: &str) -> bool {
     true
 }
 
-pub fn solve() -> String {
-    let input = include_str!("input.txt");
+fn part_1(input: &str) -> String {
     let mut ans = 0;
     for (i, line) in input.lines().enumerate() {
         if is_possible(line) {
@@ -42,4 +41,47 @@ pub fn solve() -> String {
         }
     }
     ans.to_string()
+}
+
+fn power_of_cubes(line: &str) -> u32 {
+    let (_, games) = line.split_once(": ").expect("Invalid input");
+    let mut max_blue = 0;
+    let mut max_green = 0;
+    let mut max_red = 0;
+    for round in games.split("; ") {
+        for ball in round.split(", ") {
+            let (number_str, color) = ball.split_once(' ').expect("Invalid input");
+            let number: u32 = number_str
+                .parse()
+                .unwrap_or_else(|_| panic!("Invalid input {number_str}"));
+            match color {
+                "red" => {
+                    max_red = std::cmp::max(max_red, number);
+                }
+                "green" => {
+                    max_green = std::cmp::max(max_green, number);
+                }
+                "blue" => {
+                    max_blue = std::cmp::max(max_blue, number);
+                }
+                _ => panic!("Invalid input"),
+            }
+        }
+    }
+    max_red * max_green * max_blue
+}
+
+fn part_2(input: &str) -> String {
+    let mut ans = 0;
+    for line in input.lines() {
+        ans += power_of_cubes(line);
+    }
+    ans.to_string()
+}
+
+pub fn solve() -> String {
+    let input = include_str!("input.txt");
+    let part_1_ans = part_1(input);
+    let part_2_ans = part_2(input);
+    format!("Part 1: {}\nPart 2: {}", part_1_ans, part_2_ans)
 }
