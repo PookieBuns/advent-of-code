d0e32b4caad5aa379b987453e0a246a1ad3412f3
allenyuchen <allenyuchen@hotmail.com>
2024-12-03 23:32:16 -0800

    day 4

--- src/year_2024/day_4/mod.rs
+++ src/year_2024/day_4/mod.rs
@@ -2,12 +2,141 @@ use std::path::Path;
 
 use crate::Answer;
 
-fn part_1(_input: &str) -> Option<i32> {
-    None
+fn search_for_xmas(chars: &[Vec<char>], row: usize, col: usize) -> i32 {
+    if chars[row][col] != 'X' {
+        return 0;
+    }
+    let mut count = 0;
+    // left
+    if col + 3 < chars[row].len()
+        && chars[row][col + 1] == 'M'
+        && chars[row][col + 2] == 'A'
+        && chars[row][col + 3] == 'S'
+    {
+        count += 1;
+    }
+    // right
+    if col >= 3
+        && chars[row][col - 1] == 'M'
+        && chars[row][col - 2] == 'A'
+        && chars[row][col - 3] == 'S'
+    {
+        count += 1;
+    }
+    // down
+    if row + 3 < chars.len()
+        && chars[row + 1][col] == 'M'
+        && chars[row + 2][col] == 'A'
+        && chars[row + 3][col] == 'S'
+    {
+        count += 1;
+    }
+    // up
+    if row >= 3
+        && chars[row - 1][col] == 'M'
+        && chars[row - 2][col] == 'A'
+        && chars[row - 3][col] == 'S'
+    {
+        count += 1;
+    }
+    // diagonal down right
+    if row + 3 < chars.len()
+        && col + 3 < chars[row].len()
+        && chars[row + 1][col + 1] == 'M'
+        && chars[row + 2][col + 2] == 'A'
+        && chars[row + 3][col + 3] == 'S'
+    {
+        {
+            count += 1;
+        }
+    }
+    // diagonal down left
+    if row + 3 < chars.len()
+        && col >= 3
+        && chars[row + 1][col - 1] == 'M'
+        && chars[row + 2][col - 2] == 'A'
+        && chars[row + 3][col - 3] == 'S'
+    {
+        count += 1;
+    }
+    // diagonal up right
+    if row >= 3
+        && col + 3 < chars[row].len()
+        && chars[row - 1][col + 1] == 'M'
+        && chars[row - 2][col + 2] == 'A'
+        && chars[row - 3][col + 3] == 'S'
+    {
+        count += 1;
+    }
+    // diagonal up left
+    if row >= 3
+        && col >= 3
+        && chars[row - 1][col - 1] == 'M'
+        && chars[row - 2][col - 2] == 'A'
+        && chars[row - 3][col - 3] == 'S'
+    {
+        count += 1;
+    }
+    count
 }
 
-fn part_2(_input: &str) -> Option<i32> {
-    None
+fn part_1(input: &str) -> Option<i32> {
+    let chars = input
+        .lines()
+        .map(|l| l.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+    (0..chars.len())
+        .fold(0, |acc, row| {
+            (0..chars[row].len()).fold(acc, |acc, col| acc + search_for_xmas(&chars, row, col))
+        })
+        .into()
+}
+
+fn search_for_xmas_2(chars: &[Vec<char>], row: usize, col: usize) -> i32 {
+    if chars[row][col] != 'A' {
+        return 0;
+    }
+    if row == 0 {
+        return 0;
+    }
+    if row == chars.len() - 1 {
+        return 0;
+    }
+    if col == 0 {
+        return 0;
+    }
+    if col == chars[row].len() - 1 {
+        return 0;
+    }
+    let up_left = chars[row - 1][col - 1];
+    let up_right = chars[row - 1][col + 1];
+    let down_left = chars[row + 1][col - 1];
+    let down_right = chars[row + 1][col + 1];
+    if up_left == 'M' && up_right == 'M' && down_left == 'S' && down_right == 'S' {
+        return 1;
+    }
+    if up_left == 'M' && down_left == 'M' && up_right == 'S' && down_right == 'S' {
+        return 1;
+    }
+    if down_left == 'M' && down_right == 'M' && up_left == 'S' && up_right == 'S' {
+        return 1;
+    }
+    if up_right == 'M' && down_right == 'M' && up_left == 'S' && down_left == 'S' {
+        return 1;
+    }
+    0
+}
+
+fn part_2(input: &str) -> Option<i32> {
+    let chars = input
+        .lines()
+        .map(|l| l.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+    (0..chars.len())
+        .fold(0, |acc, row| {
+            (0..chars[row].len()).fold(acc, |acc, col| acc + search_for_xmas_2(&chars, row, col))
+        })
+        .into()
 }
 pub fn solve() -> Answer {
     let cur_dir = Path::new(file!()).parent().unwrap();
