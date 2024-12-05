aba1ae6565ba8218373aa9069795dc528308708a
allenyuchen <allenyuchen@hotmail.com>
2024-12-04 22:25:23 -0800

    day 5

--- src/year_2024/day_5/mod.rs
+++ src/year_2024/day_5/mod.rs
@@ -1,13 +1,88 @@
-use std::path::Path;
+use std::{collections::HashMap, path::Path};
 
 use crate::Answer;
 
-fn part_1(_input: &str) -> Option<i32> {
-    None
+fn create_adj_mat(input: &str) -> HashMap<i32, Vec<i32>> {
+    input
+        .lines()
+        .map(|line| {
+            let (before_str, after_str) = line.split_once("|").unwrap();
+            let before: i32 = before_str.parse().unwrap();
+            let after: i32 = after_str.parse().unwrap();
+            (before, after)
+        })
+        .fold(HashMap::new(), |mut acc, (before, after)| {
+            acc.entry(before).or_default().push(after);
+            acc
+        })
 }
 
-fn part_2(_input: &str) -> Option<i32> {
-    None
+fn part_1(input: &str) -> Option<i32> {
+    let (first, second) = input.split_once("\n\n").unwrap();
+    let adj_mat = create_adj_mat(first);
+    second
+        .lines()
+        // convert to list of numbers
+        .map(|line| {
+            line.split(",")
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        // find valid lists
+        .filter(|nums| {
+            nums.iter().enumerate().all(|(i, before)| {
+                nums.iter()
+                    .skip(i + 1)
+                    .all(|&after| !adj_mat.get(&after).unwrap_or(&vec![]).contains(before))
+            })
+        })
+        // find the middle number
+        .map(|nums| nums[nums.len() / 2])
+        .sum::<i32>()
+        .into()
+}
+
+fn part_2(input: &str) -> Option<i32> {
+    let (first, second) = input.split_once("\n\n").unwrap();
+    let adj_mat = create_adj_mat(first);
+    second
+        .lines()
+        // convert to list of numbers
+        .map(|line| {
+            line.split(",")
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        // find invalid lists
+        .filter(|nums| {
+            nums.iter().enumerate().any(|(i, before)| {
+                nums.iter()
+                    .skip(i + 1)
+                    .any(|&after| adj_mat.get(&after).unwrap_or(&vec![]).contains(before))
+            })
+        })
+        // reoganize the list to be valid
+        .map(|nums| {
+            let mut q = nums.clone();
+            let mut new_nums = Vec::new();
+            while !q.is_empty() {
+                let available_nums = q
+                    .iter()
+                    .filter(|&before| {
+                        q.iter()
+                            .all(|&after| !adj_mat.get(&after).unwrap_or(&vec![]).contains(before))
+                    })
+                    .copied()
+                    .collect::<Vec<i32>>();
+                q.retain(|&x| !available_nums.contains(&x));
+                new_nums.extend(available_nums);
+            }
+            new_nums
+        })
+        // find the middle number
+        .map(|nums| nums[nums.len() / 2])
+        .sum::<i32>()
+        .into()
 }
 pub fn solve() -> Answer {
     let cur_dir = Path::new(file!()).parent().unwrap();
