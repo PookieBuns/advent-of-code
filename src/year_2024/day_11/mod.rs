13257186235f7b5672a78ebb769adf014fcc1b80
allenyuchen <allenyuchen@hotmail.com>
2024-12-11 14:18:19 +0800

    use cached crate

--- src/year_2024/day_11/mod.rs
+++ src/year_2024/day_11/mod.rs
@@ -1,4 +1,6 @@
-use std::{collections::HashMap, path::Path};
+use std::path::Path;
+
+use cached::proc_macro::cached;
 
 use crate::Answer;
 
@@ -7,33 +9,28 @@ fn part_1(input: &str) -> Option<i64> {
         .split_whitespace()
         .map(|s| s.parse::<i64>().unwrap())
         .collect::<Vec<i64>>();
-    let mut cache = HashMap::new();
     stones
         .into_iter()
-        .map(|stone| dp(stone, 25, &mut cache))
+        .map(|stone| dp(stone, 25))
         .sum::<i64>()
         .into()
 }
 
-fn dp(stone: i64, remain: i64, cache: &mut HashMap<(i64, i64), i64>) -> i64 {
+#[cached]
+fn dp(stone: i64, remain: i64) -> i64 {
     if remain == 0 {
         return 1;
     }
-    if let Some(ans) = cache.get(&(stone, remain)) {
-        return *ans;
-    }
-    let ans = match stone {
-        0 => dp(1, remain - 1, cache),
+    match stone {
+        0 => dp(1, remain - 1),
         s if (s.ilog10() + 1) % 2 == 0 => {
             let mid = (s.ilog10() + 1) / 2;
             let left = s / 10_i64.pow(mid);
             let right = s % 10_i64.pow(mid);
-            dp(left, remain - 1, cache) + dp(right, remain - 1, cache)
+            dp(left, remain - 1) + dp(right, remain - 1)
         }
-        _ => dp(stone * 2024, remain - 1, cache),
-    };
-    cache.insert((stone, remain), ans);
-    ans
+        _ => dp(stone * 2024, remain - 1),
+    }
 }
 
 fn part_2(input: &str) -> Option<i64> {
@@ -41,10 +38,9 @@ fn part_2(input: &str) -> Option<i64> {
         .split_whitespace()
         .map(|s| s.parse::<i64>().unwrap())
         .collect::<Vec<i64>>();
-    let mut cache = HashMap::new();
     stones
         .into_iter()
-        .map(|stone| dp(stone, 75, &mut cache))
+        .map(|stone| dp(stone, 75))
         .sum::<i64>()
         .into()
 }
