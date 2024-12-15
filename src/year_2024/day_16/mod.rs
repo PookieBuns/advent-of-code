77ca363356b3d6af275e58dc3a2d9a21a41587d8
allenyuchen <allenyuchen@hotmail.com>
2024-12-15 14:16:40 +0800

    scaffold 15-25

---
+++ src/year_2024/day_16/mod.rs
@@ -0,0 +1,19 @@
+use std::path::Path;
+
+use crate::Answer;
+
+fn part_1(_input: &str) -> Option<i32> {
+    None
+}
+
+fn part_2(_input: &str) -> Option<i32> {
+    None
+}
+pub fn solve() -> Answer {
+    let cur_dir = Path::new(file!()).parent().unwrap();
+    let file_path = cur_dir.join("input.txt");
+    let input = std::fs::read_to_string(file_path).unwrap();
+    let part_1 = part_1(&input);
+    let part_2 = part_2(&input);
+    Answer::from_parts(part_1, part_2)
+}
