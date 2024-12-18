d2c2feeec1f3e82a67a330fcc24bca5491c1afa5
allenyuchen <allenyuchen@hotmail.com>
2024-12-18 13:54:19 +0800

    use rayon

--- src/year_2024/day_18/mod.rs
+++ src/year_2024/day_18/mod.rs
@@ -1,5 +1,7 @@
 use std::{collections::VecDeque, path::Path};
 
+use rayon::prelude::*;
+
 use crate::Answer;
 
 const GRID_SIZE: usize = 70 + 1;
@@ -31,6 +33,7 @@ fn populate_grid(coordinates: &[Coordinate], count: usize) -> Vec<Vec<i32>> {
     grid
 }
 
+#[allow(dead_code)]
 fn print_grid(grid: &[Vec<i32>]) {
     for row in grid.iter() {
         for cell in row.iter() {
@@ -78,22 +81,29 @@ fn part_1(input: &str) -> Option<i32> {
 
 fn part_2(input: &str) -> Option<i32> {
     let coordinates = get_coordinates(input);
-    for populate_bytes in 1..=coordinates.len() {
-        let grid = populate_grid(&coordinates, populate_bytes);
-        let path = find_path(&grid);
-        if path == i32::MAX {
-            let blocking_byte = &coordinates[populate_bytes - 1];
-            println!("Blocking byte: ({blocking_byte:?}");
-            return path.into();
-        }
-    }
-    None
+    let first_block_i = (1..=coordinates.len())
+        .into_par_iter()
+        .find_first(|populate_bytes| {
+            let grid = populate_grid(&coordinates, *populate_bytes);
+            let path = find_path(&grid);
+            path == i32::MAX
+        })
+        .unwrap();
+    let blocking_byte = &coordinates[first_block_i - 1];
+    println!("Blocking byte: {blocking_byte:?}");
+    (first_block_i as i32).into()
 }
 pub fn solve() -> Answer {
     let cur_dir = Path::new(file!()).parent().unwrap();
     let file_path = cur_dir.join("input.txt");
     let input = std::fs::read_to_string(file_path).unwrap();
+    let part_1_start_time = std::time::Instant::now();
     let part_1 = part_1(&input);
+    let part_1_elapsed_time = part_1_start_time.elapsed();
+    println!("Done with part 1 in {:?}", part_1_elapsed_time);
+    let part_2_start_time = std::time::Instant::now();
     let part_2 = part_2(&input);
+    let part_2_elapsed_time = part_2_start_time.elapsed();
+    println!("Done with part 2 in {:?}", part_2_elapsed_time);
     Answer::from_parts(part_1, part_2)
 }
--- src/year_2024/day_24/mod.rs
+++ src/year_2024/day_24/mod.rs
@@ -13,7 +13,13 @@ pub fn solve() -> Answer {
     let cur_dir = Path::new(file!()).parent().unwrap();
     let file_path = cur_dir.join("input.txt");
     let input = std::fs::read_to_string(file_path).unwrap();
+    let part_1_start_time = std::time::Instant::now();
     let part_1 = part_1(&input);
+    let part_1_elapsed_time = part_1_start_time.elapsed();
+    println!("Done with part 1 in {:?}", part_1_elapsed_time);
+    let part_2_start_time = std::time::Instant::now();
     let part_2 = part_2(&input);
+    let part_2_elapsed_time = part_2_start_time.elapsed();
+    println!("Done with part 2 in {:?}", part_2_elapsed_time);
     Answer::from_parts(part_1, part_2)
 }
