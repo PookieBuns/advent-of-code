c4fc758c741e3d616863fe5f5419402edf8a5eb7
allenyuchen <allenyuchen@hotmail.com>
2024-12-22 02:00:34 +0800

    day 19 + 20

--- src/year_2024/day_19/mod.rs
+++ src/year_2024/day_19/mod.rs
@@ -1,13 +1,88 @@
-use std::path::Path;
+use std::{collections::HashMap, path::Path};
 
 use crate::Answer;
 
-fn part_1(_input: &str) -> Option<i32> {
-    None
+fn get_patterns(input: &str) -> Vec<Vec<char>> {
+    input.split(", ").map(|s| s.chars().collect()).collect()
 }
 
-fn part_2(_input: &str) -> Option<i32> {
-    None
+fn get_designs(input: &str) -> Vec<Vec<char>> {
+    input.lines().map(|line| line.chars().collect()).collect()
+}
+
+fn dp(patterns: &[Vec<char>], design: &[char], i: usize, cache: &mut HashMap<usize, bool>) -> bool {
+    if i == design.len() {
+        return true;
+    }
+    if let Some(&res) = cache.get(&i) {
+        return res;
+    }
+    for pattern in patterns {
+        if design.len() - i < pattern.len() {
+            continue;
+        }
+        if design[i..i + pattern.len()] != *pattern {
+            continue;
+        }
+        if dp(patterns, design, i + pattern.len(), cache) {
+            cache.insert(i, true);
+            return true;
+        }
+    }
+    cache.insert(i, false);
+    false
+}
+
+fn is_possible(patterns: &[Vec<char>], design: &[char]) -> bool {
+    dp(patterns, design, 0, &mut HashMap::new())
+}
+
+fn part_1(input: &str) -> Option<i64> {
+    let (patterns_str, design_str) = input.split_once("\n\n").unwrap();
+    let patterns = get_patterns(patterns_str);
+    let designs = get_designs(design_str);
+    (designs
+        .into_iter()
+        .filter(|design| is_possible(&patterns, design))
+        .count() as i64)
+        .into()
+}
+
+fn dp_2(patterns: &[Vec<char>], design: &[char], i: usize, cache: &mut HashMap<usize, i64>) -> i64 {
+    if i == design.len() {
+        return 1;
+    }
+    if let Some(&res) = cache.get(&i) {
+        return res;
+    }
+    let mut res = 0;
+    for pattern in patterns {
+        if design.len() - i < pattern.len() {
+            continue;
+        }
+        if design[i..i + pattern.len()] != *pattern {
+            continue;
+        }
+        let combos = dp_2(patterns, design, i + pattern.len(), cache);
+        res += combos;
+    }
+    cache.insert(i, res);
+    res
+}
+
+fn combos(patterns: &[Vec<char>], design: &[char]) -> i64 {
+    dp_2(patterns, design, 0, &mut HashMap::new())
+}
+
+fn part_2(input: &str) -> Option<i64> {
+    let (patterns_str, design_str) = input.split_once("\n\n").unwrap();
+    let patterns = get_patterns(patterns_str);
+    let designs = get_designs(design_str);
+    (designs
+        .into_iter()
+        .map(|design| combos(&patterns, &design))
+        .sum::<i64>())
+    .into()
 }
 pub fn solve() -> Answer {
     let cur_dir = Path::new(file!()).parent().unwrap();
--- src/year_2024/day_20/mod.rs
+++ src/year_2024/day_20/mod.rs
@@ -1,13 +1,147 @@
-use std::path::Path;
+use std::{collections::VecDeque, path::Path};
 
 use crate::Answer;
 
-fn part_1(_input: &str) -> Option<i32> {
-    None
+fn convert_to_grid(input: &str) -> Vec<Vec<char>> {
+    input.lines().map(|line| line.chars().collect()).collect()
 }
 
-fn part_2(_input: &str) -> Option<i32> {
-    None
+fn find_end(grid: &[Vec<char>]) -> (usize, usize) {
+    grid.iter()
+        .enumerate()
+        .find_map(|(i, row)| {
+            row.iter()
+                .enumerate()
+                .find_map(|(j, c)| (*c == 'E').then_some((i, j)))
+        })
+        .unwrap()
+}
+
+fn djikstra(grid: &[Vec<char>]) -> Vec<Vec<i32>> {
+    let (end_i, end_j) = find_end(grid);
+    let (n, m) = (grid.len(), grid[0].len());
+    let mut dist = vec![vec![i32::MAX; m]; n];
+    let mut q = VecDeque::new();
+    q.push_back((end_i, end_j, 0));
+    dist[end_i][end_j] = 0;
+    while let Some((i, j, d)) = q.pop_front() {
+        let dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+        for (di, dj) in dirs {
+            let ni = i.overflowing_add_signed(di).0;
+            if ni >= n {
+                continue;
+            }
+            let nj = j.overflowing_add_signed(dj).0;
+            if nj >= m {
+                continue;
+            }
+            let nd = d + 1;
+            if grid[ni][nj] == '#' {
+                continue;
+            }
+            if nd >= dist[ni][nj] {
+                continue;
+            }
+            dist[ni][nj] = nd;
+            q.push_back((ni, nj, nd));
+        }
+    }
+    dist
+}
+
+#[allow(dead_code)]
+fn print_grid(grid: &[Vec<i32>]) {
+    for row in grid {
+        for cell in row {
+            if *cell == i32::MAX {
+                print!("## ");
+            } else {
+                print!("{:02} ", cell);
+            }
+        }
+        println!();
+    }
+}
+
+fn find_cheats(grid: &[Vec<i32>], threshhold: i32) -> i32 {
+    let mut cheats = 0;
+    let (n, m) = (grid.len(), grid[0].len());
+    for i in 1..n - 1 {
+        for j in 1..m - 1 {
+            if grid[i][j] != i32::MAX {
+                continue;
+            }
+            let above = grid[i - 1][j];
+            let below = grid[i + 1][j];
+            if above != i32::MAX && below != i32::MAX {
+                let time_saved = (above - below).abs() - 2;
+                if time_saved >= threshhold {
+                    // println!("({}, {}): {}", i, j, time_saved);
+                    cheats += 1;
+                }
+            }
+            let left = grid[i][j - 1];
+            let right = grid[i][j + 1];
+            if left != i32::MAX && right != i32::MAX {
+                let time_saved = (left - right).abs() - 2;
+                if time_saved >= threshhold {
+                    // println!("({}, {}): {}", i, j, time_saved);
+                    cheats += 1;
+                }
+            }
+        }
+    }
+    cheats
+}
+
+fn part_1(input: &str) -> Option<i32> {
+    let grid = convert_to_grid(input);
+    let dist = djikstra(&grid);
+    // println!("{:?}", dist);
+    // print_grid(&dist);
+    find_cheats(&dist, 100).into()
+}
+
+fn find_cheats_2(grid: &[Vec<i32>], threshhold: i32, cheat_time: usize) -> i32 {
+    let mut cheats = 0;
+    let (n, m) = (grid.len(), grid[0].len());
+    for from_i in 1..n - 1 {
+        for from_j in 1..m - 1 {
+            if grid[from_i][from_j] == i32::MAX {
+                continue;
+            }
+            for to_i in from_i..std::cmp::min(from_i + 1 + cheat_time, n - 1) {
+                let cheat_time_remain = cheat_time - (to_i - from_i);
+                // for to_j in std::..std::cmp::min(from_j + 1 + cheat_time_remain, m - 1) {
+                let mut to_j_start = std::cmp::max(1, from_j.saturating_sub(cheat_time_remain));
+                if to_i == from_i {
+                    to_j_start = from_j;
+                }
+                let to_j_end = std::cmp::min(from_j + cheat_time_remain, m - 1);
+                for to_j in to_j_start..=to_j_end {
+                    if grid[to_i][to_j] == i32::MAX {
+                        continue;
+                    }
+                    let manhattan =
+                        (to_i as i32 - from_i as i32).abs() + (to_j as i32 - from_j as i32).abs();
+                    // println!("({from_i},{from_j})->({to_i},{to_j}), manhattan: {manhattan}");
+                    let race_diff = (grid[from_i][from_j] - grid[to_i][to_j]).abs();
+                    let time_saved = race_diff - manhattan;
+                    if time_saved >= threshhold {
+                        cheats += 1;
+                    }
+                }
+            }
+        }
+    }
+    cheats
+}
+
+fn part_2(input: &str) -> Option<i32> {
+    let grid = convert_to_grid(input);
+    let dist = djikstra(&grid);
+    // print_grid(&dist);
+    find_cheats_2(&dist, 100, 20).into()
 }
 pub fn solve() -> Answer {
     let cur_dir = Path::new(file!()).parent().unwrap();
